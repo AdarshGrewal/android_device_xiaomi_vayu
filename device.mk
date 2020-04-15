81da2a9bf7bde3050948ee60abb35ca385a0b731
Arian <arian.kulmer@web.de>
2020-04-15 13:16:40 +0200

    vayu: camera: Build camera provider
    Change-Id: Ief92b2adfec0e365c0a73181cbf4b40c95b36070

--- device.mk
+++ device.mk
@@ -25,6 +25,10 @@ TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
 # Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \

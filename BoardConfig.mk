73f393f9fbf13d13a0285ba38d37739318bd72a3
Arian <arian.kulmer@web.de>
2021-09-02 19:12:09 +0200

    vayu: gps: Import from LA.UM.9.1.r1-06700-SMxxx0.0
    - Cleanup Unused GPS files
    - Build GNSS HAL
    - Import GPS configs
    
    Co-authored-by: raghavt20 <raghavtandon2000@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,6 +65,10 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 # FM
 BOARD_HAVE_QCOM_FM := true
 
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+LOC_HIDL_VERSION := 4.0
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml

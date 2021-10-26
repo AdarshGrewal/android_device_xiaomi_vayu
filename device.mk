5ffb894832cf0e0f85a9c305e9b6b9b9f75457a6
raghavt20 <raghavtandon2000@gmail.com>
2021-10-27 01:40:14 +0530

    vayu: fingerprint: Build vendor variant of libkeymaster_messages

--- device.mk
+++ device.mk
@@ -138,6 +138,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.vayu
 
+PRODUCT_PACKAGES += \
+    libkeymaster_messages.vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 

c0e066898bc331922599c1f0a3fc4a860b57879d
Adarshgrewal <adarshgrewal@gmail.com>
2021-10-31 02:16:14 +0530

    vayu: vibrator: Build service

--- device.mk
+++ device.mk
@@ -121,6 +121,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan

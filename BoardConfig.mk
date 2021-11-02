6b40d33f01c9c084a39748c80ce1ec6cf4030eac
Arian <arian.kulmer@web.de>
2021-11-02 22:27:15 +0530

    vayu: FM: Build FM2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,9 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
+# FM
+BOARD_HAVE_QCOM_FM := true
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml

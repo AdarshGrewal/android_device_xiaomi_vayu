1143dcc369dc4ce9b83c1f408c3f4d17bb415057
AdarshGrewal <adarshgrewal@gmail.com>
2022-01-18 12:15:17 +0530

    vayu: Allow building with prebuilt ELF files

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,7 @@ BOARD_VENDOR := xiaomi
 DEVICE_PATH := device/xiaomi/vayu
 
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Inherit from proprietary files
 include vendor/xiaomi/vayu/BoardConfigVendor.mk
